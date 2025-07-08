d407f042184920412b4e9d8ac2234173e892468e
Matt Heusser <matt.heusser@gmail.com>
2025-07-08 10:12:57 -0700

    Made activity 1-3, not 1-2

---
+++ 1-3/compile-1-3-windows.bat
@@ -0,0 +1,11 @@
+#----------------------------------------#
+# Compile-1-3.bat - A batch script
+#
+# 
+#This code will compile your c++
+g++ 1-3convert.cpp -o CS630-1-3.exe
+#----------------------------------------#
+#Here's how to run your c++ under windows
+#CS630-1-3.exe
+#
+#
